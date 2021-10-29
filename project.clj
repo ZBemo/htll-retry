ea831677710c034e20a432dbfc59d791f20ca27e
Zachary Petti <zachmpetti@gmail.com>
2021-10-29 17:52:28 -0400

    Switch to 0-clause bsd license.
    I'm not a big fan of the EPL or whatever it set up for me.

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject htll "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "0BSD"
+            :url "https://spdx.org/licenses/0BSD.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
   :main ^:skip-aot htll.core
   :target-path "target/%s"
